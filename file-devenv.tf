4a9c28608e25536d813f8bbcd461395acfa69c90
Will Beebe <will@containerlabs.io>
2021-02-20 10:17:06 +0000

    bump

--- file-devenv.tf
+++ file-devenv.tf
@@ -1,6 +1,6 @@
 module "devenv" {
   source                 = "container-labs/devenv-repo/github"
-  version                = "~> 0.9"
+  version                = "~> 1.0"
   repository             = var.repository
   workspace_image        = "nodejs"
   workspace_image_tag    = var.workspace_image_tag
